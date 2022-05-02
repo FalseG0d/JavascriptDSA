7c300fb0e45ac13d5d52cefffb183a2ae8e2553a
FalseG0d <garg.apoorv1098@gmail.com>
2022-05-02 22:35:38 +0530

    SpringAngular

---
+++ employeemanager/employeeManager/src/main/java/tech/getarrays/employeeManager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package tech.getarrays.employeeManager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.getarrays.employeeManager.exception.EmployeeNotFoundException;
+import tech.getarrays.employeeManager.model.Employee;
+import tech.getarrays.employeeManager.repo.EmployeeRepo;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo){
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(Employee employee){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return EmployeeRepo.findEmployeeByID(id)
+                .orElseThrow(()->new EmployeeNotFoundException("Employee: " + id + " Not Found"));
+    }
+
+    public void deleteEmployee(Long id){
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
