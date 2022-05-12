98691ab99df3a4653fdfa1b073f15eaf0f323d18
FalseG0d <garg.apoorv1098@gmail.com>
2022-05-12 08:00:50 +0530

    Base App

--- TaskTracker/angular-crash/src/app/components/tasks/tasks.component.ts
+++ TaskTracker/angular-crash/src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-tasks';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -8,11 +8,30 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>this.tasks=tasks);
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+
+  addTask(task: Task){
+    this.taskService.addTask(task).subscribe((task)=>(this.tasks.push(task)));
+  }
+
+  toggleTaskReminder(task: Task) {
+    task.reminder = !task.reminder;
+
+    this.taskService.updateTaskReminder(task).subscribe();
   }
 
 }
