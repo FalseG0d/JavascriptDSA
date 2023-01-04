38e8245ae8a85a7110bf87d7df0c068bf92cf190
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-01-04 23:08:59 +0530

    Minor

--- SumZero.js
+++ SumZero.js
@@ -10,14 +10,12 @@ for(let num of input) {
     if(map.has(num)) map.set(num, map.get(num)+1);
     else map.set(num, 1);
 }
-
 for(let num of input) {
     if(num != 0 && map.has(-num)) {
         res.push([num, -num]);
         map.delete(num);
         }
 }
-
 for(let arr of res) {
     console.log(arr[0], arr[1]);
 }
